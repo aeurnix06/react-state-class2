56b11a6ea71719722bed3d7b992ab13ca929a5eb
aeurnix06 <hamaurnix@gmail.com>
2026-06-29 20:05:23 +0530

    Complete React Lottery game and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import LudoBoard from './LudoBord'
 import TodoList from './TodoList'
+import Lottery from './Lottery'
+import {sum} from './helper'
 
-function App() {
-  return (
+function App() {     
+
+  let winCondition = (ticket) => {
+    return ticket[0] === 0;
+  };
+
+  return (    
     <>
-    <TodoList/>
+    <Lottery n={3} winningSum={15} winCondition={winCondition}/>
+
+
     </>
   )
 }
---
+++ src/Ticket.jsx
@@ -0,0 +1,15 @@
+import TicketNum from "./TicketNum";
+import "./Ticket.css";
+
+export default function Ticket({ticket}){
+    return(
+        <div className="Ticket">
+            <p>Ticket</p>
+            <br />
+            {ticket.map((num, idx) =>(
+                <TicketNum num={num} key={idx}/>
+            ))}
+
+        </div>
+    )
+}
